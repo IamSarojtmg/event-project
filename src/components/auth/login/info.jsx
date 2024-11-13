f2d85a9128231dc0b275f4e3c8cda81905638764
Saroj Tamang <sarojtmg992@gmail.com>
2024-11-13 11:42:56 +0000

    updated btn

--- src/components/auth/login/info.jsx
+++ src/components/auth/login/info.jsx
@@ -1,21 +1,18 @@
 import { useState } from "react";
 
 const AdminLogin = () => {
-  // Initial email and password states
   const [email, setEmail] = useState("northcoders@gmail.com");
   const [password, setPassword] = useState("northcoders123");
 
-  // State to control visibility of the email
   const [showEmail, setShowEmail] = useState(false);
 
   function handleClick() {
-    // Toggle the visibility of the email
     setShowEmail(!showEmail);
   }
 
   return (
     <>
-      <button onClick={handleClick}>Log in details</button>
+      <button className="show-info" onClick={handleClick}>Unlock the Secrets</button>
       {showEmail && <div>{email}</div>} 
       {showEmail && <div>{password}</div>} 
 
